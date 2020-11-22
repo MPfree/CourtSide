7015c3ffad186be96893d29ad9c5073a7d89b39e
Liam Marengere <marenge1@WIN.ecf.utoronto.ca>
2020-11-21 19:12:50 -0500

    Posting capabilities INTEGRATED

---
+++ mie350webapp/src/com/mie/model/Post.java
@@ -0,0 +1,210 @@
+package com.mie.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.util.HashMap;
+
+import com.mie.util.DbUtil;
+
+public class Post {
+	
+	private Connection conn;
+	
+	private int postID;
+	private int playerID;
+	private int courtID;
+	private String title;
+	private String description;
+	
+	public Connection getConn() {
+		return conn;
+	}
+
+	public void setConn(Connection conn) {
+		this.conn = conn;
+	}
+
+	public int getPostID() {
+		return postID;
+	}
+
+	public void setPostID(int postID) {
+		this.postID = postID;
+	}
+
+	public int getPlayerID() {
+		return playerID;
+	}
+
+	public void setPlayerID(int playerID) {
+		this.playerID = playerID;
+	}
+
+	public int getCourtID() {
+		return courtID;
+	}
+
+	public void setCourtID(int courtID) {
+		this.courtID = courtID;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public image getPostPic() {
+		return postPic;
+	}
+
+	public void setPostPic(image postPic) {
+		this.postPic = postPic;
+	}
+
+	public int getProps() {
+		return props;
+	}
+
+	public void setProps(int props) {
+		this.props = props;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+	private image postPic;
+	private int props;
+	private double rating;
+	
+	public Post(int postID, int playerID, int courtID, String title, 
+			String description, image postPic, int props, double rating) {
+		this.postID = postID;
+		this.playerID = playerID;
+		this.courtID = courtID;
+		this.title = title;
+		this.description = description;
+		this.postPic = postPic;
+		this.props = props;
+		this.rating = rating;
+	}
+	
+	public Post() {
+		
+	}
+	
+	
+	public void makePost() {
+		conn = DbUtil.getConnection();
+		try {
+			int postID = generatePostID();
+			
+			if (postID != 0) {
+				PreparedStatement makePostPrep = conn.prepareStatement("insert into Social (PropID,CourtID,PlayerID,Title,Description,Post_Image"
+						+ ",Props,Rating values (?,?,?,?,?,?,?,?)");
+				makePostPrep.setInt(1, postID);
+				makePostPrep.setInt(2, this.courtID);
+				makePostPrep.setInt(3, this.playerID);
+				makePostPrep.setString(4, this.title);
+				makePostPrep.setString(5, this.description);
+//				makePostPrep.setImage(6, this.postPic);
+				makePostPrep.setInt(7, this.props);
+				makePostPrep.setDouble(8, this.rating);
+				
+				makePostPrep.execute();
+			}else {
+				// not good but not sure if this would ever happen
+			}
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+
+		}
+		
+	}
+	
+	public HashMap<Integer,Post> getCourtPosts(int courtID) {
+		conn = DbUtil.getConnection();
+		try {
+			// Creates a hashmap where the key is the postID and the value is the Post object
+			HashMap<Integer,Post> posts = new HashMap<Integer,Post>();
+			
+			String getPostQuery = "Select * from Social where CourtID = " + courtID + " order by PostID desc";
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery(getPostQuery);
+			
+			while(rs.next()) {
+				posts.put(rs.getInt(1), new Post(rs.getInt(1), courtID, rs.getInt(3), rs.getString(4),rs.getString(5),rs.getImage(6),rs.getInt(7),rs.getDouble(8)));
+			}
+			
+			return posts;
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+
+		}
+	}
+	
+	public int generatePostID() {
+		conn = DbUtil.getConnection();
+		try {
+			
+			String generateIDQuery = "select count(PostID) from Social";
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery(generateIDQuery);
+			
+			return rs.getInt(1)+1;
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+	
+	public int getProps(int postID) {
+		conn = DbUtil.getConnection();
+		try {
+			String getProps = "select Props from Social where PostID = "+postID;
+			Statement stProps = conn.createStatement();
+			ResultSet rs = stProps.executeQuery(getProps);
+			
+			return rs.getInt(1);
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+	
+	public void updateProps(int postID) {
+		conn = DbUtil.getConnection();
+		try {
+			
+			String updateProps = "update Social set Props = "+ (getProps(postID)+1) + "where PostID = " + postID;
+			Statement st = conn.createStatement();
+			st.execute(updateProps);
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
--- mie350webapp/src/main.java
+++ mie350webapp/src/main.java
@@ -20,30 +20,5 @@ public class main {
 		
 	}
 	
-	public void makePost(int postID, int playerID, int courtID, String title, 
-			String description, image postPic, int props, double rating) {
-		conn = DbUtil.getConnection();
-		try {
-			
-			PreparedStatement makePostPrep = conn.prepareStatement("insert into Social (PropID,CourtID,PlayerID,Title,Description,Post_Image"
-					+ ",Props,Rating values (?,?,?,?,?,?,?,?)");
-			makePostPrep.setInt(1, postID);
-			makePostPrep.setInt(2, courtID);
-			makePostPrep.setInt(3, playerID);
-			makePostPrep.setString(4, title);
-			makePostPrep.setString(5, description);
-//			makePostPrep.setImage(6, postPic);
-			makePostPrep.setInt(7, props);
-			makePostPrep.setDouble(8, rating);
-			
-			makePostPrep.execute();
-
-			
-		}catch(SQLException e) {
-			e.printStackTrace();
-
-		}
-		
-	}
 
 }
